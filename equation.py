148c57d13f05864a2fd9601cd165879637cde12a
Douglas Allen <doal7262@colorad.edu>
2020-05-11 16:02:03 -0600

    First Draft

--- equation.py
+++ equation.py
@@ -1,25 +1,53 @@
 
+def alphaDepth(LHS, RHS, result):
+    for n in range(len(RHS)):
+        if RHS[n].isalpha():
+            if RHS[n] == LHS:
+                print('infinite loop')
+                return 'error'
+
+            elif LHS not in eqDict:
+                print('Variable not found')
+                return 'error'
+
+            elif RHS[n] in pruner:
+                result += pruner[RHS[n]]
+
+            else:
+                result += alphaDepth(RHS[n], eqDict[RHS[n]], 0)
+
+        else:
+            result += int(RHS[n])
+
+    pruner[LHS] = result
+    return result
+
 testEq = ['offset = 4 + random + 1', 'location = 1 + origin + offset', 
           'origin = 3         + 5', 'random = 2']
 
 eqDict = {}
 
-while (testEq):
-    LHS = testEq[0].split(' =')[0]
-    RHS = testEq[0].split(' =')[1]
+# Breaking down the strings into something machine readable
+for n in range(len(testEq)):
+    # eqDict[LHS] = RHS
+    LHS = testEq[n].split(' =')[0]
+    RHS = testEq[n].split(' =')[1]
     RHS = RHS.replace('+', ' ')
     RHS = RHS.split()
     eqDict[LHS] = RHS
-    print(eqDict)
-    testEq.remove(testEq[0])
 
-'''
-for m in range(len(sumEq)):
-    if sumEq[m].isalpha():
-        print(sumEq[m])
+
+# Depth First Search Approach
+eqStack, ans, pruner = sorted(list(eqDict.keys())), [], {}
+
+while(eqStack):
+    eqAns = alphaDepth(eqStack[0], eqDict[eqStack[0]], 0)
+    if eqAns == 'error':
+        print('equation name encountered')
+        break
     else:
-        sumEq[m] = int(sumEq[m])
+        ans.append(eqStack[0] + ' = ' + str(eqAns))
 
-    print(sumEq)
-'''
-print(eqDict)
+    eqStack.pop(0)
+
+print(ans)
